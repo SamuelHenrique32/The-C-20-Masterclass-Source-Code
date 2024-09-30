0638441dda31583125e9ed0ee167b21bf53ccc40
Samuel Henrique Dalmas <samuel.dalmas@mobitec.com.br>
2024-09-30 09:20:12 -0300

    chore: Studies on Section 13

--- 13.Pointers/13.10PointerArithmetic_Navigation/main.cpp
+++ 13.Pointers/13.10PointerArithmetic_Navigation/main.cpp
@@ -11,7 +11,7 @@ int main(){
     
     int * p_score { scores};
     
-    /*
+
     std::cout << "Values in scores array (p_score pointer increment) : " << std::endl;
 	
     std::cout << "Address : " << p_score << " value : " <<  *(p_score) << std::endl;
@@ -44,17 +44,17 @@ int main(){
 	std::cout << "Address : " << p_score << " value : " <<  *(p_score) << std::endl;
 	++p_score; // Moves froward by sizeof(int) : 4 bytes
 	std::cout << std::endl;
-    */
 
-   /*
+
+
 	std::cout << std::endl;
 	std::cout << "Explicit addition of integer : " << std::endl;
 	p_score = scores; // Reset the pointer to the start of the array
 	std::cout << "scores[4] : " << *(p_score + 4) << std::endl; // Moves forward by 4 * sizeof(int)
-	*/
+
 
     //Can use loops to print these things out : easier
-    /*
+
     p_score = scores;
     
 	std::cout << std::endl;
@@ -62,12 +62,12 @@ int main(){
     for ( size_t i{0} ; i < std::size(scores) ; ++i){
         std::cout <<"Value : "<< *(p_score + i) << std::endl; // scores[i]
     }
-    */
+
 
 
     //Can also do arithmetic on the array name
     //just like any pointer.
-    /*
+
     p_score = scores;
     
 	std::cout << std::endl;
@@ -75,10 +75,10 @@ int main(){
     for ( size_t i{0} ; i < std::size(scores) ; ++i){
         std::cout <<"Value : "<< *(scores + i) << std::endl;
 	}
-    */
+
 
     // Can Print the elements in reverse order
-    /*
+
 	std::cout<< std::endl;
     std::cout << "Elements in reverse order with decrementing pointer arithmetic: " << std::endl;
     for ( size_t i{std::size(scores)} ; i > 0 ; --i){
@@ -86,10 +86,10 @@ int main(){
 																//scores is already pointing to the first
 																//element in the array.
     }
-    */
+
 
 	//Print in reverse order with -- operator on p_score
-    /*
+
 	std::cout<< std::endl;
     std::cout << "Elements in reverse order -- arithmetic on the p_scores pointer: " << std::endl;
     p_score = scores + std::size(scores) -1;
@@ -98,16 +98,16 @@ int main(){
 																//scores is already pointing to the first
 																//element in the array.
     }
-    */
+
 
     // Can Print the elements in reverse order with array index
-    /*
+
 	std::cout<< std::endl;
     std::cout << "Elements in reverse order with array index notation: " << std::endl;
     for ( size_t i{std::size(scores)} ; i > 0 ; --i){
         std::cout <<"Value : "<< scores [ i -1] << std::endl;
     }
-    */
+
 
     p_score = scores;
     
